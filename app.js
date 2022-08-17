6c2d20bc8523b098ee30e208b2226084c40d03db
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-08-16 20:28:09 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-/*var numCard = document.getElementById("card-number")
-var namUser=document.getElementById("user-name")
+var numCard = document.getElementById("card-number")
+var namUser = document.getElementById("user-name")
+var expCard = document.getElementById("date-expedition")
+var cvc = document.getElementById("cvc")
 
 numCard.innerHTML = "0000 0000 0000 0000"
-namUser.innerHTML=""*/
+namUser.innerHTML = "JANE APPLASEED"
+expCard.innerHTML = " 00/00"
+cvc.innerHTML = " 000"
+
+function confirm() {
+    var name = document.getElementById("name-user").value
+    var numberCard = document.getElementById("number-card").value
+    var month = document.getElementById("month").value
+    var year = document.getElementById("year").value
+    var cvc = document.getElementById("cvc").value
+
+    numCard.innerHTML = numberCard
+    namUser.innerHTML = name
+    expCard.innerHTML = month + "/" + year
+    cvc.innerHTML = cvc
+}
