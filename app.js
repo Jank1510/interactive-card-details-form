4710c9f0d29cfc3e5e43596166863d9dd775ff9d
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-08-17 20:42:56 -0500

    update

--- app.js
+++ app.js
@@ -8,15 +8,44 @@ namUser.innerHTML = "JANE APPLASEED"
 expCard.innerHTML = " 00/00"
 cvc.innerHTML = " 000"
 
-function confirm() {
-    var name = document.getElementById("name-user").value
-    var numberCard = document.getElementById("number-card").value
-    var month = document.getElementById("month").value
-    var year = document.getElementById("year").value
-    var cvc = document.getElementById("cvc").value
+var bordeError = ' 2px solid hsl(270, 3%, 87%)'
 
-    numCard.innerHTML = numberCard
-    namUser.innerHTML = name
-    expCard.innerHTML = month + "/" + year
-    cvc.innerHTML = cvc
+function confirm() {
+    //datos de la tarjeta
+    var name = document.getElementById("name-user")
+    var numberCard = document.getElementById("number-card")
+    var month = document.getElementById("month")
+    var year = document.getElementById("year")
+    var cvc2 = document.getElementById("cvc")
+        //datos de los inputs
+    var errorName = document.getElementById("error-name")
+    var errorNumber = document.getElementById("error-number")
+    var errorMm = document.getElementById("error-mm")
+    var errorYy = document.getElementById("error-yy")
+    var errorCvc = document.getElementById("error-cvc")
+
+
+    if (name.value != '') {
+        if (numberCard.value != '') {
+            if (month.value = !'' && year.value != '') {
+                if (cvc2 != '') {
+                    numCard.innerHTML = numberCard.value
+                    namUser.innerHTML = name.value
+                    expCard.innerHTML = month.value + "/" + year.value
+                    cvc.innerHTML = cvc2.value
+                }
+            }
+
+        } else {
+            errorNumber.innerHTML = "Can't be blank"
+            numberCard
+        }
+        errorName.innerHTML = ""
+        name.style.border = bordeError
+    } else {
+        errorName.innerHTML = "Can't be blank"
+        name.style.border = ' 2px solid hsl(0, 100%, 66%)'
+    }
+
+
 }
