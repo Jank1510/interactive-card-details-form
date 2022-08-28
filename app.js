c2f53bcfccdbbcc443aa9f447e53c239105485ae
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-08-27 20:27:09 -0500

    update

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var namUser = document.getElementById("user-name")
 var expCard = document.getElementById("date-expedition")
 var cvc = document.getElementById("cvc")
 
-numCard.innerHTML = "0000 0000 0000 0000"
+var formulario = document.getElementById("formulario")
+var completado = document.getElementById("completado")
+
+numCard.innerHTML = "00000000000000"
 namUser.innerHTML = "JANE APPLASEED"
 expCard.innerHTML = " 00/00"
 cvc.innerHTML = " 000"
 
-var bordeError = ' 2px solid hsl(270, 3%, 87%)'
+formulario.style.display = 'flex'
+completado.style.display = 'none'
+
+var bordeErrorInactivo = ' 2px solid hsl(270, 3%, 87%)'
+var bordeErrorActivo = ' 2px solid hsl(0, 100%, 66%)'
 
 function confirm() {
     //datos de la tarjeta
@@ -16,35 +23,60 @@ function confirm() {
     var numberCard = document.getElementById("number-card")
     var month = document.getElementById("month")
     var year = document.getElementById("year")
-    var cvc2 = document.getElementById("cvc")
+    var cvc2 = document.getElementById("cvc2")
         //datos de los inputs
     var errorName = document.getElementById("error-name")
     var errorNumber = document.getElementById("error-number")
     var errorMm = document.getElementById("error-mm")
-    var errorYy = document.getElementById("error-yy")
     var errorCvc = document.getElementById("error-cvc")
 
 
     if (name.value != '') {
+        errorName.innerHTML = ""
+        name.style.border = bordeErrorInactivo
         if (numberCard.value != '') {
-            if (month.value = !'' && year.value != '') {
-                if (cvc2 != '') {
-                    numCard.innerHTML = numberCard.value
-                    namUser.innerHTML = name.value
-                    expCard.innerHTML = month.value + "/" + year.value
-                    cvc.innerHTML = cvc2.value
-                }
-            }
+            errorNumber.innerHTML = ""
+            numberCard.style.border = bordeErrorInactivo
+            if (isNaN(numberCard.value) == false) {
+                errorNumber.innerHTML = ""
+                numberCard.style.border = bordeErrorInactivo
+                if (month.value != '' && year.value != '') {
+                    errorMm.innerHTML = ""
+                    month.style.border = bordeErrorInactivo
+                    year.style.border = bordeErrorInactivo
+                    if (cvc2.value != '') {
+                        errorCvc.innerHTML = ""
+                        cvc2.style.border = bordeErrorInactivo
 
+
+                        numCard.innerHTML = numberCard.value
+                        namUser.innerHTML = name.value
+                        expCard.innerHTML = month.value + "/" + year.value
+                        cvc.innerHTML = cvc2.value
+
+
+                        formulario.style.display = 'none'
+                        completado.style.display = 'flex'
+                    } else {
+                        errorCvc.innerHTML = "Can't be blank"
+                        cvc2.style.border = bordeErrorActivo
+                    }
+                } else {
+                    errorMm.innerHTML = "Can't be blank"
+                    month.style.border = bordeErrorActivo
+                    year.style.border = bordeErrorActivo
+                }
+            } else {
+                errorNumber.innerHTML = "Wrong format, numbers only"
+                numberCard.style.border = bordeErrorActivo
+            }
         } else {
             errorNumber.innerHTML = "Can't be blank"
-            numberCard
+            numberCard.style.border = bordeErrorActivo
         }
-        errorName.innerHTML = ""
-        name.style.border = bordeError
     } else {
         errorName.innerHTML = "Can't be blank"
-        name.style.border = ' 2px solid hsl(0, 100%, 66%)'
+        name.style.border = bordeErrorActivo
     }
 
 
